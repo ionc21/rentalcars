0d72ba0b33895c68fe78dac10d93bd2e0b398e14
ion.ciobanu <ion.ciobanu@jit.team>
2019-04-18 15:27:49 +0200

    avoid null checks

---
+++ src/main/java/com/rentalcars/utils/ResolveNestedNullChecks.java
@@ -0,0 +1,21 @@
+package com.rentalcars.utils;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+@UtilityClass
+public class ResolveNestedNullChecks {
+
+
+    public static <T> T resolve(Supplier<T> resolver) {
+        try {
+            T result = resolver.get();
+            return Optional.ofNullable(result).orElse((T) "");
+        }
+        catch (NullPointerException e) {
+            return (T) "";
+        }
+    }
+}
